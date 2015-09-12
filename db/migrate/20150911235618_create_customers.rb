d2333b6bd4eac9e6a689d84d6852b3c28c1aaeae
下向和紀 <simo@simo3.local>
2015-09-12 09:08:35 +0900

    Model,indexの設定

---
+++ db/migrate/20150911235618_create_customers.rb
@@ -0,0 +1,16 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      #「,」に続けて「空入力はダメ」、「20文字数」
+      t.string :family_name, null: false,length: {maximum: 20}
+      t.string :given_name,  null: false,length: {maximum: 20}
+      t.string :email,       null: false
+
+      t.timestamps null: false
+    end
+    #
+#検索されやすいように目次を設定
+  add_index :customers, :email, unique: true
+  end
+
+end
